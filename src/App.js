af85d14476fdc850261ec25868a0465c20fcc9ba
MStreet3 <mi.street@gmail.com>
2019-07-29 17:17:39 -0400

    complete header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 
@@ -13,10 +14,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
-        <Route path="/shop" component={ShopPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
