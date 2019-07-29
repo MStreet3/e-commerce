d4843743eb95d72c373fc7fbc5359a70ebfbf43e
MStreet3 <mi.street@gmail.com>
2019-07-29 14:34:38 -0400

    setup initial styling of menu

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+
+function App() {
+  return <HomePage />;
+}
+
+export default App;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.styles.scss';
+import DirectoryMenu from '../../components/directory-menu/directory-menu.component';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <DirectoryMenu />
+    </div>
+  );
+};
+
+export default HomePage;
