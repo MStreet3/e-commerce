ad5c414ba36031e7bc19166c88b4327dbeda4899
Michael Street <mi.street@gmail.com>
2019-08-01 17:33:34 -0400

    Updates

---
+++ precache-manifest.1bf53b5760f3f06ae757d6350cd909d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9016340432f117a1135d7ba4edd0d7ea",
+    "url": "/e-commerce-demo/index.html"
+  },
+  {
+    "revision": "f7545afe2a2953843599",
+    "url": "/e-commerce-demo/static/css/main.323f6b85.chunk.css"
+  },
+  {
+    "revision": "a82d6d7011592e8f9f64",
+    "url": "/e-commerce-demo/static/js/2.7a42625e.chunk.js"
+  },
+  {
+    "revision": "f7545afe2a2953843599",
+    "url": "/e-commerce-demo/static/js/main.1e43075c.chunk.js"
+  },
+  {
+    "revision": "b90efc54ba5eee563ea7",
+    "url": "/e-commerce-demo/static/js/runtime~main.5b85aba3.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce-demo/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/e-commerce-demo/static/media/shoppingBag.09e6f5e7.svg"
+  }
+]);
